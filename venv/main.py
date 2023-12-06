dd5e8e40a840feab7fd77ca64f79b85826da5421
Jimmy Vu <jimmyvu1357@gmail.com>
2023-12-05 23:26:40 -0800

    Added background music

--- venv/main.py
+++ venv/main.py
@@ -7,8 +7,8 @@ pygame.init()
 ###Score (ms since pygame.init())
 def display_score():
   current_time = int(pygame.time.get_ticks() / 1000) - start_time
-  score_surf = font.render(f'Score: {current_time}', False, 'White')
-  score_rect = score_surf.get_rect(center = (500, 53))
+  score_surf = font.render(f'Score: {current_time}', False, 'Black')
+  score_rect = score_surf.get_rect(center = (500, 83))
   screen.blit(score_surf, score_rect)
 
 ###Game set up
@@ -18,6 +18,8 @@ font = pygame.font.Font('Minecraft.ttf', 30)
 lostFont = pygame.font.Font('Minecraft.ttf', 50)
 game_active = True
 start_time = 0
+instructions_surf = font.render("Avoid the capybaras and oranges!", False, 'Black')
+instructions_rect = instructions_surf.get_rect(center=(500, 40))
 
 ###Background model and position
 background_surf = pygame.image.load('background1.png').convert_alpha()
@@ -40,6 +42,9 @@ ground_surface_rect4 = ground_surface.get_rect(midbottom = (1200, 542))
 
 ###Jump sound effect
 jump_noise = pygame.mixer.Sound('JumpEffect.wav')
+music = pygame.mixer.Sound('CuteCircus.wav')
+music.set_volume(0.1)
+music.play()
 
 ###Enemey model and position
 enemy = pygame.image.load('capyhole.png').convert_alpha()
@@ -49,16 +54,16 @@ enemy_rect = enemy.get_rect(bottomleft = (1200, 403))
 oranges = ["orange1.png","orange2.png","orange3.png"]
 enemy1 = pygame.image.load(random.choice(oranges)).convert_alpha()
 enemy1 = pygame.transform.rotozoom(enemy1,0, 0.065)
-enemy_rect1 = enemy1.get_rect(bottomleft = (1800, 200))
+enemy_rect1 = enemy1.get_rect(bottomleft = (1100, 200))
 
 ###Player model and position
 player = pygame.image.load('capybara1.png').convert_alpha()
 player = pygame.transform.rotozoom(player,0, 0.1)
+player_rect = player.get_rect(midbottom = (85,385))
 player1 = pygame.image.load('capybara2.png').convert_alpha()
 player1 = pygame.transform.rotozoom(player1,0, 0.1)
-
-player_rect = player.get_rect(midbottom = (85,385))
 player_rect1 = player1.get_rect(midbottom = (85,385))
+
 player_gravity = 0
 randomSpeed = random.randint(13,17)
 
@@ -158,9 +163,11 @@ while True:
     if enemy_rect1.colliderect(player_rect) or enemy_rect1.colliderect(player_rect1):
       game_active = False
 
+    screen.blit(instructions_surf, instructions_rect)
     display_score()
     pygame.display.update()
     lastScore = int(pygame.time.get_ticks() / 1000) - start_time
+
   ###Lost screen
   else:
     screen.fill((135, 206, 235))
@@ -173,11 +180,11 @@ while True:
     screen.blit(ground_surface, ground_surface_rect3)
     screen.blit(ground_surface, ground_surface_rect4)
 
-    endMessage_surf = lostFont.render("You lost!", False, 'White')
+    endMessage_surf = lostFont.render("You lost!", False, 'Black')
     endMessage_rect = endMessage_surf.get_rect(center=(500, 170))
-    endMessage_surf1 = lostFont.render("Press space to try again", False, 'White')
+    endMessage_surf1 = lostFont.render("Press space to try again", False, 'Black')
     endMessage_rect1 = endMessage_surf1.get_rect(center=(500, 230))
-    scoreMessage_surf = lostFont.render(f"Your score: {lastScore}", False, 'White')
+    scoreMessage_surf = lostFont.render(f"Your score: {lastScore}", False, 'Black')
     scoreMessage_rect = scoreMessage_surf.get_rect(center=(500, 295))
 
     screen.blit(enemy, enemy_rect)
